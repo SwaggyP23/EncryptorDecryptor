7ace632f77a7a74672e21e8310b0c62278c71ae1
Reda Fakih <redafakih.99@gmail.com>
2022-06-19 01:48:44 +0300

    Fixed the main function

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,20 @@ int main()
 	Encryptor::Init("res/Key.txt");
 	Decryptor::Init("res/Key.txt");
 
-	std::string Encrypted = Encryptor::Get().EncryptMessage("Hello Reda, how are you mate(?) All good and you? last day hasn't been so good for me i lost 500$");
+	std::string message;
+	std::cout << "Enter your message: ";
+	getline(std::cin, message);
+
+	std::string Encrypted = Encryptor::Get().EncryptMessage(message);
 	LOG_INFO("Encrypted message is: {0}", Encrypted);
 
-	std::string origin = Decryptor::Get().DecryptMessage(Encrypted);
-	LOG_INFO("Original message is: {0}", origin);
+	char i;
+	std::cout << "Do u want to decrypt it? [Y/n]";
+	std::cin >> i;
+	if (i == 'Y') {
+		std::string origin = Decryptor::Get().DecryptMessage(Encrypted);
+		LOG_INFO("Original message is: {0}", origin);
+	}
 
 	std::cin.get();
 }
